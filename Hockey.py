9f2c09796f33636830b4929cb7cc038a56891d2f
SwagElliot <elliot.felleki.917@edu.leksand.se>
2024-02-13 09:59:07 +0100

    Update Hockey.py

--- Hockey.py
+++ Hockey.py
@@ -1,23 +1,28 @@
 import random
 
 målchans = int(input("Hur stor är chansen i procent att spelaren gör mål en viss match?"))
-mållösa_matcher = int(input("Hur många mål lösa matcher i rad vill du kolla efter?"))
-sessonger = 10000
+mållösStreak = int(input("Hur många mål lösa matcher i rad vill du kolla efter?"))
+antalSessonger = 10000
 matcher = 52
 streak = 0
 mål = 0
-antal_tillfällen = 0
+antalStreaks = 0
 
 
 
-for i in range(sessonger):
+for i in range(antalSessonger):
     for i in range(matcher):
-        mål = random.randint(0,100)
-        if målchans > mål:
-            streak = streak +1
+        mål = random.randrange(0,100)
+        if målchans <= mål:
+            streak += 1
         else:
             streak = 0
-        if streak == mållösa_matcher:
-            antal_tillfällen +=1
+        if streak == mållösStreak:
+            antalStreaks += 1
+            streak = 0
+            break
+            
+            
 
-print("Spelaren var mållös i ",mållösa_matcher," matcher i rad",antal_tillfällen / sessonger," procent av sessongerna.")
+print("Spelaren var mållös i ",mållösStreak," matcher i rad",antalStreaks / (antalSessonger * 0.01)," procent av sessongerna.")
+print(antalStreaks)
